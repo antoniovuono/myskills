15f5b04377744d089a1d3f3f45b68a80b3c67b21
Antonio Vuono <vuono@mswebmaker.com>
2021-06-30 22:36:04 -0300

    Second title added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
 export function Home() {
   return (
     <>
       <View style={styles.container}>
         <Text style={styles.title}>Welcome, Antonio!</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="New skill"
+          placeholderTextColor="#555"
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+
+        <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
       </View>
     </>
   );
@@ -24,6 +43,26 @@ const styles = StyleSheet.create({
   title: {
     color: '#fff',
     fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
     fontWeight: 'bold',
   },
 });
